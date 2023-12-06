2f650be7d0c275e52324dcae3673ead63946a1f1
Antonio Blázquez <antonio.blazquez@videoslots.com>
2023-12-06 17:38:46 +0100

    solved day 5, that was hard

--- day5/main.go
+++ day5/main.go
@@ -1,166 +1,133 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
-type FromToMap struct {
-	source_start      int
-	destination_start int
-	delta             int
+type Range struct {
+	a int
+	b int
+	c int
+}
+
+type SeedRange struct {
+	start int
+	end   int
 }
 
 func main() {
-	maps := make(map[string]map[string][]FromToMap)
-
-	file, err := os.Open("test.txt")
+	body, err := os.ReadFile("input.txt")
 
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
-	defer file.Close()
+	before, after, _ := bytes.Cut(body, []byte("\n\n"))
 
-	filescanner := bufio.NewScanner(file)
+	parts := bytes.Split(after, []byte("\n\n"))
 
-	filescanner.Split(bufio.ScanLines)
+	// split seeds by ":" and take the first value
+	before = bytes.Split(before, []byte(": "))[1]
 
-	seeds := []int{}
+	// split seeds by " " as int array
+	inputs, _ := byteToIntArray(before)
+	seeds := []SeedRange{}
+	for i := 0; i < len(inputs); i += 2 {
+		seeds = append(seeds, SeedRange{start: inputs[i], end: inputs[i] + inputs[i+1]})
+	}
 
-	from, to := "", ""
+	// fmt.Println("seeds: ", seeds)
 
-	for filescanner.Scan() {
-		// does the line starts with "seeds:" ? if yes, print the line
-		line := filescanner.Text()
-		if len(line) > 6 && line[0:6] == "seeds:" {
-			// split the line by space
-			splited_line := strings.Split(line, " ")
-			// convert the string to int
-			for _, seed := range splited_line[1:] {
-				number, err := strconv.Atoi(seed)
-				if err != nil {
-					panic(err)
+	found := false
+
+	for _, part := range parts {
+		ranges := []Range{}
+		for _, line := range bytes.Split(part, []byte("\n"))[1:] {
+			r, _ := byteToIntArray(line)
+			ranges = append(ranges, Range{a: r[0], b: r[1], c: r[2]})
+		}
+
+		new := []SeedRange{}
+
+		for len(seeds) > 0 {
+			// pop first element
+			seed := seeds[0]
+			seeds = seeds[1:]
+			// fmt.Println("seed: ", seed)
+			// fmt.Println("ranges: ", ranges)
+
+			found = false
+
+			for _, r := range ranges {
+				// fmt.Println("range: ", r)
+				var overlap_start int
+				if seed.start < r.b {
+					overlap_start = r.b
+				} else {
+					overlap_start = seed.start
 				}
-				seeds = append(seeds, number)
-			}
+				var overlap_end int
+				if seed.end < r.b+r.c {
+					overlap_end = seed.end
+				} else {
+					overlap_end = r.b + r.c
+				}
 
-			fmt.Println("seeds: ", seeds)
-		}
+				// fmt.Println("overlap_start: ", overlap_start)
+				// fmt.Println("overlap_end: ", overlap_end)
 
-		// we have the seeds, now we need to create the maps
-		// there are lines starting with "somethig-to-something map:" that we need to parse
-		if len(line) > 6 && line[len(line)-4:] == "map:" {
-			from = line[0:strings.Index(line, "-")]
-			to = line[strings.Index(line, "-")+4 : len(line)-5]
-			// fmt.Println("from: ", from, " to: ", to)
-			// create the map
-			maps[from] = make(map[string][]FromToMap)
-			// create the map for the destination
-			maps[from][to] = []FromToMap{}
-		}
-
-		// if the line starts with a digit, we need to parse it
-		if len(line) > 0 && line[0] >= '0' && line[0] <= '9' {
-			// split the line by space
-			splited_line := strings.Split(line, " ")
-			if len(splited_line) != 3 {
-				continue
-			}
-			source_start, err := strconv.Atoi(splited_line[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			destination_start, err := strconv.Atoi(splited_line[0])
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			delta, err := strconv.Atoi(splited_line[2])
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			maps[from][to] = append(maps[from][to], FromToMap{
-				source_start:      source_start,
-				destination_start: destination_start,
-				delta:             delta,
-			})
-		}
-
-	}
-
-	seeds2 := []struct {
-		start int
-		end   int
-	}{}
-	for k, v := range seeds {
-		if k%2 == 1 {
-			continue
-		}
-
-		if k+1 < len(seeds) {
-			seeds2 = append(seeds2, struct {
-				start int
-				end   int
-			}{
-				start: v,
-				end:   v + seeds[k+1],
-			})
-		}
-	}
-	fmt.Print("seeds2: ", seeds2, "\n")
-
-	to_resource := 0
-	lowest := 9999999999
-	for _, seed := range seeds2 {
-		fmt.Print()
-		for i := seed.start; i < seed.end; i++ {
-			from = "seed"
-			to = "soil"
-			to_resource = i
-			// while to != "location"
-			for {
-				//prev_to_resource = to_resource
-				to_resource = getToResource(from, to, to_resource, maps)
-				//fmt.Println("from: ", from, " to: ", to, " seed: ", prev_to_resource, " to_resource: ", to_resource)
-				from = to
-				if to == "location" {
+				if overlap_start < overlap_end {
+					new = append(new, SeedRange{start: overlap_start - r.b + r.a, end: overlap_end - r.b + r.a})
+					found = true
+					if overlap_start > seed.start {
+						seeds = append(seeds, SeedRange{start: seed.start, end: overlap_start})
+					}
+					if seed.end > overlap_end {
+						seeds = append(seeds, SeedRange{start: overlap_end, end: seed.end})
+					}
 					break
 				}
-				var firstKey string
-				for key := range maps[from] {
-					firstKey = key
-					break
-				}
-				to = firstKey
-
 			}
 
-			if to_resource < lowest {
-				lowest = to_resource
+			if !found {
+				new = append(new, seed)
 			}
+
+		}
+		seeds = new
+	}
+
+	if len(seeds) == 0 {
+		fmt.Println("No elements in the slice")
+		return
+	}
+
+	// fmt.Println("seeds: ", seeds)
+
+	s := seeds[0]
+	min := s.start
+	for _, value := range seeds[1:] {
+		if value.start < min {
+			min = value.start
 		}
 	}
 
-	fmt.Println("lowest: ", lowest)
+	fmt.Println(min)
 }
 
-func getToResource(from string, to string, seed int, maps map[string]map[string][]FromToMap) int {
-	// get the map
-	m := maps[from][to]
-
-	// fmt.Println("from: ", from, " to: ", to, " seed: ", seed, " map: ", m)
-	// get the destination
-	for _, v := range m {
-		if seed >= v.source_start && seed <= (v.source_start+v.delta) {
-			return v.destination_start + (seed - v.source_start)
+func byteToIntArray(input []byte) ([]int, error) {
+	parts := bytes.Split(input, []byte(" "))
+	result := []int{}
+	for _, part := range parts {
+		seed, err := strconv.Atoi(string(part))
+		if err != nil {
+			return nil, err
 		}
+		result = append(result, seed)
 	}
-	return seed
+	return result, nil
 }
